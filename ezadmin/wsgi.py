02ea093f6571b438a0b2eedeb1afc51a8242c2a6
rikhsan <rakhmad.ikhsanudin@gmail.com>
2018-07-09 20:58:46 +0700

    asd

--- ezadmin/wsgi.py
+++ ezadmin/wsgi.py
@@ -11,16 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# sys.path.append('/opt/ezadmin')
-# sys.path.append('/opt/ezadmin/env/lib/python2.7/site-packages')
-# sys.path.append('/usr/lib64/python27.zip')
-# sys.path.append('/usr/lib64/python2.7')
-# sys.path.append('/usr/lib64/python2.7/plat-linux2')
-# sys.path.append('/usr/lib64/python2.7/lib-tk')
-# sys.path.append('/usr/lib64/python2.7/lib-old')
-# sys.path.append('/usr/lib64/python2.7/lib-dynload')
-# sys.path.append('/usr/lib64/python2.7/site-packages')
-# sys.path.append('/usr/lib/python2.7/site-packages')
+sys.path.append('/opt/ezadmin')
+sys.path.append('/opt/ezadmin/env/lib/python2.7/site-packages')
+sys.path.append('/usr/lib64/python27.zip')
+sys.path.append('/usr/lib64/python2.7')
+sys.path.append('/usr/lib64/python2.7/plat-linux2')
+sys.path.append('/usr/lib64/python2.7/lib-tk')
+sys.path.append('/usr/lib64/python2.7/lib-old')
+sys.path.append('/usr/lib64/python2.7/lib-dynload')
+sys.path.append('/usr/lib64/python2.7/site-packages')
+sys.path.append('/usr/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ezadmin.settings")
 
